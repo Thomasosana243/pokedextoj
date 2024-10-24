648a3273abbdcf2142416a7ee8741ddbf4588ec2
Thomasosana243 <thomasjosueosana@gmail.com>
2024-10-24 14:10:17 +0200

    maj3

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -9,5 +9,14 @@ function PokemonCard() {
 		</figure>
 	);
 }
-
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+	},
+];
 export default PokemonCard;
